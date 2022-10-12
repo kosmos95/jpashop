fcf32b6a10f28f8ddcba3da486710fb4b1252794
deakun ko <rheorjs789@gmail.com>
2022-10-12 10:41:21 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 <회원 도메인 개발 - 회원 리포지토리 개발>
    jpql

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -18,10 +18,17 @@ public class MemberRepository {
         em.persist(member);
     }
     public Member findOne(Long id) {
-        return em.find(Member.class, id);
+
+        return em.find(Member.class, id); //단건 조회
+    }
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)//jpql ql의 대상이 테이블이 아니라 entity
+                .getResultList(); 
     }
 
-//    public List<Member> findAll() {
-//        em.createQuery("select m from Member m", Member.class)
-//    }
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
 }
