8b05a62af4deb3834c9055f236808b6c8d005cf0
deakun ko <rheorjs789@gmail.com>
2022-10-21 17:37:03 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 <주문 도메인 개발 - 주문 검색 기능 개발>

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +19,17 @@ public class OrderRepository {
 
     public Order findOne (Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m"+
+                "where o.status = :status" +
+                "and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus()) //파라미터 바인딩?
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) //범위 제한 최대 1000개까지 조회
+                .getResultList();
+
     }
 
 }
