09a9b5e719869b2f9173f7651ac7e11100751a62
deakun ko <rheorjs789@gmail.com>
2022-10-24 14:09:15 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 <웹 계층 개발 - 회원 등록> @NotEmpty 어노테이션 인식성공

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -3,9 +3,10 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.Valid;
 
-@Getter @Setter @Valid
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
 public class MemberForm {
 
     @NotEmpty(message="회원 이름은 필수 입니다")
