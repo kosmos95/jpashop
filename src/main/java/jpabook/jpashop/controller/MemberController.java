0bc8dda2080312d7aa8d0bbccdc77dcaff6c44b8
deakun ko <rheorjs789@gmail.com>
2022-10-25 09:50:40 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 <웹 계층 개발 - 회원 등록> Error validation 기능

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new") //Get은 폼 화면을 열어본다.
+    public String createForm(Model model) {  //model은 데이터를 실어서 넘기는 역할을 한다.
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "members/new") //Post는 데이터를 등록하는 것
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+}
