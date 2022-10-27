528b82408513d36271ab2be8e15d6fa9abd3f708
deakun ko <rheorjs789@gmail.com>
2022-10-27 13:05:07 +0900

    스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 <웹 계층 개발 - 상품주문>
    상품 주문 코드작성

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId, //form name으로 지정된 값이 sumit으로 오면 value 값이 너머온다
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
